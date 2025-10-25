bf8770577ae396936940f06a940dc07105d4b7c1
syedkarim99 <syedkarim2122@gmail.com>
2025-10-26 01:00:04 +0800

    initial commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "prescriptionWebapp"; // must match what you created in phpMyAdmin
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("❌ Database connection failed: " . $conn->connect_error);
+}
+// echo "✅ Connected successfully!";
+?>
