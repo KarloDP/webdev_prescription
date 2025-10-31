51f1ce5aede99014bfe7b8bad5d35c7b05e8e86b
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2025-10-31 16:05:28 +0800

    merged changes to karlos branch

--- actions/add_stock.php
+++ actions/add_stock.php
@@ -1,19 +1,17 @@
 <?php
 include '../db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $medicationID = $_POST['medicine_name'];
-    $genericName = $_POST['generic_name'];
-    $form = $_POST['form'];
-    $strength = $_POST['strength'];
-    $manufacturer = $_POST['manufacturer'];
+$medicationID = $_POST['medicine_name'];
+$genericName = $_POST['generic_name'];
+$form = $_POST['form'];
+$strength = $_POST['strength'];
+$manufacturer = $_POST['manufacturer'];
 
-    $sql = "INSERT INTO stock (medicine_name, generic_name, form, strength, manufacturer) VALUES ('$medicationID', '$genericName', '$form', '$strength', '$manufacturer')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New stock added successfully.";
-    } else {
-        echo "Error: " . $conn->error;
-    }
+$sql = "INSERT INTO stock (medicine_name, generic_name, form, strength, manufacturer) VALUES ('$medicationID', '$genericName', '$form', '$strength', '$manufacturer')";
+if ($conn->query($sql) === TRUE) {
+    echo "New stock added successfully.";
+} else {
+    echo "Error: " . $conn->error;
 }
 
 $conn->close();
