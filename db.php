a14bdb0e37558ef7157f6e7843e26b905d1fa829
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2025-10-31 13:55:47 +0800

    changed ip address to 127.0.0.1 from localhost in db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
+$servername = "127.0.0.1"; //try localhost if this causes an error
 $username = "root";
 $password = "";
 $database = "prescriptionWebapp"; // must match what you created in phpMyAdmin
@@ -7,7 +7,7 @@ $database = "prescriptionWebapp"; // must match what you created in phpMyAdmin
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("❌ Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 // echo "✅ Connected successfully!";
 ?>
