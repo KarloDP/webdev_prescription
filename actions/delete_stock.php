e1630984a42e8a442b54c14c78c3da3ae5f5d409
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2025-10-31 16:13:14 +0800

    moved script from dp.php to db_connect.php

--- actions/delete_stock.php
+++ actions/delete_stock.php
@@ -1,15 +1,13 @@
 <?php
 include '../db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-    $medicationID = $_POST['medicationID'];
+$medicationID = $_POST['medicationID'];
 
-    $sql = "DELETE FROM stock WHERE medicationID = '$medicationID'";
-    if ($conn->query($sql) === TRUE) {
-        echo "Stock deleted successfully.";
-    } else {
-        echo "Error: " . $conn->error;
-    }
+$sql = "DELETE FROM stock WHERE medicationID = '$medicationID'";
+if ($conn->query($sql) === TRUE) {
+    echo "Stock deleted successfully.";
+} else {
+    echo "Error: " . $conn->error;
 }
 
 $conn->close();
