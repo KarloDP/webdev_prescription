fbba16f19aa9be3074e85a02b33d5a19607963b0
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2025-11-21 12:25:34 +0800

    made a template for dispenserecord_table.php

--- backend/sql_handler/dispenserecord_table.php
+++ backend/sql_handler/dispenserecord_table.php
@@ -1,3 +1,20 @@
 <?php
 // handles add data retrieval and insertion to the database
+include(__DIR__ . '/../includes/db_connect.php');
+include(__DIR__.'/../includes/auth.php');
+
+function respond($data, $statusCode = 200) {
+    http_response_code($statusCode);
+    echo json_encode($data);
+    exit;
+}
+$method = $_SERVER['REQUEST_METHOD'];
+
+$user = require_user();
+$role = $user['role'];
+$userID = (int)$user['id'];
+
+if ($method === 'GET') {}
+else if ($method === 'POST') {}
+else if ($method === 'DELETE') {}
 ?>
