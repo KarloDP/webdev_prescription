c0dd13d4f0cf59fb70c12487bcf78ef5244751f3
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2025-12-16 18:42:43 +0800

    removed admin from branch

--- admin/routes/prescriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { showPrescriptions, showHistory } = require('../controllers/prescriptionController');
-
-router.get('/prescriptions', showPrescriptions);
-router.get('/prescriptions/:id/history', showHistory);
-
-module.exports = router;
